4d15bdaa5abfa5b6f494675f276f580bace7b9e0
FlavioAndre <flavioandre.fla@gmail.com>
2021-05-07 23:00:06 -0300

    ajuste na segurança cors

--- desafio-aws-backend/src/main/java/com/desafio/cognito/SecurityConfiguration.java
+++ desafio-aws-backend/src/main/java/com/desafio/cognito/SecurityConfiguration.java
@@ -38,6 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedOrigins(Arrays.asList("https://www.desafio-bootcamp-aws.cf","https://desafio-bootcamp-aws.cf"));
         configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
