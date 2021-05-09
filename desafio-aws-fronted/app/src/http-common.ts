09ecd11d814f1e36255cfab3440f7b2d66888136
FlavioAndre <flavioandre.fla@gmail.com>
2021-05-09 19:17:12 -0300

    ajuste no logo

--- desafio-aws-fronted/app/src/http-common.ts
+++ desafio-aws-fronted/app/src/http-common.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: {"X-Requested-With": "XMLHttpRequest"}
 });
 
