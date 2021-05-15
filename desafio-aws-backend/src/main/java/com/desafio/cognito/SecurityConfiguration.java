e7e2e0fbe81d8ed62dacdd1950b79de943856cc8
FlavioAndre <flavioandre.fla@gmail.com>
2021-05-14 22:07:58 -0300

    ajustes nos dockerfile

--- desafio-aws-backend/src/main/java/com/desafio/cognito/SecurityConfiguration.java
+++ desafio-aws-backend/src/main/java/com/desafio/cognito/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedOrigins(Arrays.asList("https://www.desafio-bootcamp-aws.cf","https://desafio-bootcamp-aws.cf"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost","https://www.desafio-bootcamp-aws.cf","https://desafio-bootcamp-aws.cf"));
         configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
